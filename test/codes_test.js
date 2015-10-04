5ecb56ed636947e9948271afdf224f467a08e346
Andri Möll <andri@dot.ee>
2015-10-04 10:44:00 +0300

    Name Oolong "O".

--- test/codes_test.js
+++ test/codes_test.js
@@ -1,11 +1,11 @@
-var $ = require("oolong")
+var O = require("oolong")
 var CODES_JSON = require("../codes.json")
 var STATUS_CODES = require("http").STATUS_CODES
 
 describe("codes.json", function() {
   // Test for superset rather than equivalence because future Node versions
   // might _add_ status codes. I presume they won't remove any though.
-  $.each(STATUS_CODES, function(name, code) {
+  O.each(STATUS_CODES, function(name, code) {
     it("must have " + code + " equal " + name, function() {
       CODES_JSON[code].must.equal(name)
     })
